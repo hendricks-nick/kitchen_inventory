2b59ac3b6faa72466c1745caffde0b036f213877
Nick Hendricks <hendricksn@byui.edu>
2019-06-25 17:34:50 -0600

    Node JS setup

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+  app.use(express.static("public"));
+  app.set("views", "views");
+  app.set("view engine", "ejs");
+
+  app.get("/", function(req, res) {
+     res.render("pages/index");
+    });
+    
+  app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
