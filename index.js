983b3a8126f9226920798399200011919669f3af
Nick Hendricks <nick.hendricks@infusionsoft.phx>
2019-06-28 13:47:27 -0600

    db update - attempt to query data 1

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const { Pool } = require("pg");
 
 const PORT = process.env.PORT || 5000;
-const connectionString = process.env.DATABASE_URL || 'postgres://ijjuidmmqrirwi:9096a2ef7618b6f1f2dc375f8c6cfc80fb0f53b16697e829113f8197ccb56926@ec2-107-21-216-112.compute-1.amazonaws.com:5432/d51ubhiu0qlh7g?ssl=true';
+const connectionString = 'postgres://ijjuidmmqrirwi:9096a2ef7618b6f1f2dc375f8c6cfc80fb0f53b16697e829113f8197ccb56926@ec2-107-21-216-112.compute-1.amazonaws.com:5432/d51ubhiu0qlh7g?ssl=true';
 
 const pool = new Pool({connectionString: connectionString});
 
@@ -24,6 +24,7 @@ const pool = new Pool({connectionString: connectionString});
       // Log this to the console for debugging purposes.
       console.log("Back from DB with result:");
       console.log(result.rows);
+      pool.end();
     }); 
     res.render("pages/index");
   });  
