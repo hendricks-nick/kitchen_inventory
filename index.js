061f662b875a37223e9a77556fd7f1b705780f94
Nick Hendricks <nick.hendricks@infusionsoft.phx>
2019-06-28 13:26:34 -0600

    db update, css update

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const { Pool } = require("pg");
+
 const PORT = process.env.PORT || 5000;
 const connectionString = process.env.DATABASE_URL || 'postgres://ijjuidmmqrirwi:9096a2ef7618b6f1f2dc375f8c6cfc80fb0f53b16697e829113f8197ccb56926@ec2-107-21-216-112.compute-1.amazonaws.com:5432/d51ubhiu0qlh7g?ssl=true';
 
