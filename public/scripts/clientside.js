209e1219d27405b6ee7c4098b2fc4761a9060133
Nick Hendricks <nick.hendricks@infusionsoft.phx>
2019-07-03 08:03:47 -0600

    loop adjustment

--- public/scripts/clientside.js
+++ public/scripts/clientside.js
@@ -11,20 +11,21 @@ function searchByItem() {
     $.get("/items", {itemType: itemType}, function(data){
         console.log("Server query complete.");
         console.log(data);
-        
 
     $(".bodyOptions").replaceWith(
-        "<div></div>" +
-        "<div></div>" +
-        "<div><h2>Name</h2></div>" +
-        "<div><h2>Quantity</h2></div>" +
-        "<div><h2>Location</h2></div>" +
-        "<div><h2>Notes</h2></div>" +
-        "<div></div>" +
-        "<div></div>");
+        '<div class="bodyOptions">' +
+        '<div></div>' +
+        '<div></div>' +
+        '<div><h2>Name</h2></div>' +
+        '<div><h2>Quantity</h2></div>' +
+        '<div><h2>Location</h2></div>' +
+        '<div><h2>Notes</h2></div>' +
+        '<div></div>' +
+        '<div></div>' +
+        '</div>');
 
 
-        for(var i = 0; i < data.list.length; i++) {
+       for(var i = 0; i < data.list.length; i++) {
             $(".bodyOptions").appendTo(
                 "<div></div>" +
                 "<div></div>" +
