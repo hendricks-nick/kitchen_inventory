61c5e4b8a6b6932fe340fb85b67ae60e929d5a36
Nick Hendricks <hendricksn@byui.edu>
2019-07-03 07:08:33 -0600

    loop

--- public/scripts/clientside.js
+++ public/scripts/clientside.js
@@ -12,7 +12,26 @@ function searchByItem() {
         console.log("Server query complete.");
         console.log(data);
         
-    $(".bodyOptions").replaceWith("<% include ../partials/test.ejs %>");
-    
+    $(".bodyOptions").replaceWith(
+        "<div></div>" +
+        "<div></div>" +
+        "<div><h2>Name</h2></div>" +
+        "<div><h2>Quantity</h2></div>" +
+        "<div><h2>Location</h2></div>" +
+        "<div><h2>Notes</h2></div>" +
+        "<div></div>" +
+        "<div></div>");
+
+        for(var i = 0; i < data.list.length(); i++) {
+            $(".bodyOptions").appendTo(
+                "<div></div>" +
+                "<div></div>" +
+                "<div><h2>" + data.list.name[i] + "</h2></div>" +
+                "<div><h2>" + data.list.qty[i] + "</h2></div>" +
+                "<div><h2>" + data.list.location_id[i] + "</h2></div>" +
+                "<div><h2>" + data.list.other_notes[i] + "</h2></div>" +
+                "<div></div>" +
+                "<div></div>");
+        }
     });
 }
